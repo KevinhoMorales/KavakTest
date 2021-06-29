b1aa3045fecd5dd7829661cc6105568c4668365c
Kevin Morales <kmorales@technisys.com>
2021-06-29 09:41:41 -0500

    Project setup

---
+++ KavakTest/KavakTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KavakTest
+//
+//  Created by Kevin Morales on 6/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
